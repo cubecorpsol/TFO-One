0961d24b21779c18fc24a32f1424366a21349ff1
bhavatarini-04-S <bhavatarini.s.aiml.2024@snsct.org>
2026-07-01 09:10:00 +0530

    Fix syntax error - missing closing brace in onClick handler

--- src/App.jsx
+++ src/App.jsx
@@ -4487,7 +4487,7 @@ function EmployeeProfile({ emp, t, lang, startEditEmployee, removeEmployee, navi
           <button className="btn btn-secondary" style={{ flex: 1 }} onClick={() => setBottomSheet('attendance')}>
             <i className="ti ti-calendar-user"></i> Mark Attendance
           </button>
-          <button className="btn btn-primary" style={{ flex: 1 }} onClick={() => { showToast('Wages calculated'); navigateTo('payroll'); }>
+          <button className="btn btn-primary" style={{ flex: 1 }} onClick={() => { showToast('Wages calculated'); navigateTo('payroll'); }}
             <i className="ti ti-wallet"></i> Pay Salary
           </button>
         </div>
@@ -5064,3 +5064,5 @@ function SettingsPage({ db, setDb, t, lang, setLang, exportAllData, showToast, n
     </div>
   );
 }
+
+
